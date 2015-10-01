625f869b6798c15fce6b33ddb88ff96d600c0d20
Vincent Chan <chanv@rohan.sdsu.edu>
2015-09-30 20:33:15 -0700

    Create axpb_tb.v

---
+++ lab1/axpb_tb.v
@@ -0,0 +1,16 @@
+/* Testbench for axpb.v */
+
+module axpb(
+    input signed [6:0] x,
+	 output signed [11:0] out 
+    );
+
+	 
+	 wire [10:0] multOut;
+	 multiply #(.L1(4),.L2(7)) 
+			mul(.in1(-(4'd3)), .in2(x), .out(multOut));
+	 add #(.L1(11), .L2(4))
+			addr(.in1(multOut), .in2(4'd3), .out(out));
+
+
+endmodule
