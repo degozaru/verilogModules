2b6dbb13d472eff24044ad5fa6207462917a04e8
Vincent Chan <chanv@rohan.sdsu.edu>
2015-10-07 16:19:11 -0700

    Create sll.v

---
+++ modules/sll.v
@@ -0,0 +1,15 @@
+/* Module: sll (logical shift left)
+ * Description: shifts logically left by in2 bits
+ * Parameters: L1 - length of input 1
+ *             L2 - length of input 2
+ */
+
+module subu        #(parameter L1 = 8,
+                    parameter L2 = 8)
+                    
+                    (input [L1-1:0] in1,
+                    input [L2-1:0] in2,
+                    output wire [L1-1:0] out);
+                    
+  assign out = in << in2;
+endmodule
